708397fc898519587d43f19c084f0733a5312bca
BenMulligan22 <mulliganba22@brightonk12.com>
2021-05-13 08:33:04 -0400

    Ill have a number nine large, a number six with extra dip, two number 45's, one with cheese, a number 7, and a large soda.

---
+++ src/main/creatures/Otter.java
@@ -0,0 +1,11 @@
+package creatures;
+
+public class Otter extends AquaticMammal {
+    public void method2(){
+        System.out.println("Paws");
+    }
+
+    public String toString(){
+        return"Beach Dweller";
+        }
+}
